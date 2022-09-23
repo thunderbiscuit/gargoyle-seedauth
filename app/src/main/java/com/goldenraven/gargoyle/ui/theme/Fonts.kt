99edff2d0613cf417cd402d08638b8c3b6f14b93
thunderbiscuit <thunderbiscuit@protonmail.com>
2022-09-23 15:05:54 -0400

    Add Outfit font

---
+++ app/src/main/java/com/goldenraven/gargoyle/ui/theme/Fonts.kt
@@ -0,0 +1,12 @@
+package com.goldenraven.gargoyle.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.goldenraven.gargoyle.R
+
+val Outfit = FontFamily(
+    Font(R.font.outfit_regular, FontWeight.Normal),
+    Font(R.font.outfit_medium, FontWeight.Medium),
+    Font(R.font.outfit_semibold, FontWeight.SemiBold),
+)

a41abbe8aaa9e72b543f94c335204680c00d21e9
thunderbiscuit <thunderbiscuit@protonmail.com>
2022-09-28 11:52:12 -0400

    Add logins screen title

--- app/src/main/java/com/goldenraven/gargoyle/ui/LoginsScreen.kt
+++ app/src/main/java/com/goldenraven/gargoyle/ui/LoginsScreen.kt
@@ -10,15 +10,55 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ChainStyle
+import androidx.constraintlayout.compose.ConstraintLayout
+import com.goldenraven.gargoyle.ui.theme.GargoyleTypography
 
 @Composable
 internal fun LoginsScreen() {
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = Color(0xffbbaabb)
+    ConstraintLayout(
+        modifier = Modifier
+            .fillMaxSize()
     ) {
-        Text(
-            "Menu Screen"
-        )
+        val (title, body) = createRefs()
+        val verticalChain = createVerticalChain(title, body, chainStyle = ChainStyle.SpreadInside)
+
+        Column(
+
+            modifier = Modifier
+                .padding(top = 48.dp)
+                .constrainAs(title) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.absoluteLeft)
+                    end.linkTo(parent.absoluteRight)
+                    bottom.linkTo(body.top)
+                }
+        ) {
+            // Title
+            Text(
+                text = "Login History",
+                style = GargoyleTypography.headlineSmall,
+                color = Color(0xff1f0208),
+                modifier = Modifier
+                    .padding(start = 32.dp, end = 32.dp, bottom = 8.dp)
+            )
+            Text(
+                text = "These are the domains you have previously logged into with this keychain",
+                style = GargoyleTypography.bodyMedium,
+                color = Color(0xff787878),
+                modifier = Modifier
+                    .padding(start = 32.dp, end = 32.dp)
+            )
+        }
+        Surface(
+            modifier = Modifier
+                .constrainAs(body) {
+                    top.linkTo(title.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                    bottom.linkTo(parent.bottom)
+                },
+        ) {}
     }
 }

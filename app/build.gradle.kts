2e77c99ed9ada4d5962b9cf03a1f64abcb723c2f
thunderbiscuit <thunderbiscuit@protonmail.com>
2022-09-30 11:59:58 -0400

    Update Android dependencies and target SKD

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.goldenraven.gargoyle"
         minSdk = 26
-        targetSdk = 32
+        targetSdk = 33
         versionCode = 1
         versionName = "v0.0.1-SNAPSHOT"
 
@@ -48,17 +48,17 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
     implementation("androidx.activity:activity-ktx:1.6.0")
     implementation("androidx.appcompat:appcompat:1.5.1")
 
     // Compose
     implementation("androidx.activity:activity-compose:1.6.0")
-    implementation("androidx.compose.animation:animation:1.1.1")
-    implementation("androidx.compose.ui:ui:1.1.1")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.1.1")
+    implementation("androidx.compose.animation:animation:1.2.1")
+    implementation("androidx.compose.ui:ui:1.2.1")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.2.1")
     implementation("androidx.compose.material3:material3:1.0.0-beta03")
-    implementation("androidx.navigation:navigation-compose:2.5.0")
+    implementation("androidx.navigation:navigation-compose:2.5.2")
     implementation("com.google.accompanist:accompanist-navigation-animation:0.23.1")
     implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
 
@@ -75,9 +75,9 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.1.1")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.1.1")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.1.1")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.2.1")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.2.1")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.2.1")
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
